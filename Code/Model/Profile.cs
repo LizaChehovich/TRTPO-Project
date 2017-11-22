4b5e27565e95e14df3cd82a1c828ccc3ccbb2bc9
LizaChehovich <lizachehovich@gmail.com>
2017-11-22 17:20:35 +0300

    Add Profile class

---
+++ Code/Model/Profile.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RSS_Reader.Model
+{
+    class Profile
+    {
+        public List<string> ResourcesList { get; set; }
+        public List<string> IncludeFiltersList { get; set; }
+        public List<string> ExcludeFiltersList { get; set; }
+    }
+}
