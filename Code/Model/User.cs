ec145a2e8e6fa03240b76487fc72c0cf1a5402c2
LizaChehovich <lizachehovich@gmail.com>
2017-11-22 17:21:09 +0300

    Add User class

---
+++ Code/Model/User.cs
@@ -0,0 +1,9 @@
+﻿namespace RSS_Reader.Model
+{
+    class User
+    {
+        public string Name { get; set; }
+        public Status Status { get; set; }
+        public Profile Profile { get; set; }
+    }
+}
