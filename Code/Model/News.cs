843f439617a830c57015953af2457c4dfef15f45
LizaChehovich <lizachehovich@gmail.com>
2017-11-22 19:16:32 +0300

    Change property name

--- Code/Model/News.cs
+++ Code/Model/News.cs
@@ -6,6 +6,6 @@
         public string Link { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
-        public string PubDate { get; set; }
+        public string PublicationDate { get; set; }
     }
 }
