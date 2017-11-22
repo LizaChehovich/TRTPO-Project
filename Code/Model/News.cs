de305f0fea5d8beb93e29ee4d06dea30a156e65b
LizaChehovich <lizachehovich@gmail.com>
2017-11-22 17:20:22 +0300

    Add News class

---
+++ Code/Model/News.cs
@@ -0,0 +1,11 @@
+﻿namespace RSS_Reader.Model
+{
+    class News
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string PubDate { get; set; }
+    }
+}
