d1ed4ecc474cf72e3c99a0f7d4011acbb4afa6d1
LizaChehovich <lizachehovich@gmail.com>
2017-11-29 04:18:16 +0300

    Add support rss version 1.0

--- Code/Controller/NewsLoader.cs
+++ Code/Controller/NewsLoader.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text.RegularExpressions;
@@ -9,6 +10,12 @@ namespace RSS_Reader.Controller
 {
     class NewsLoader
     {
+        public delegate void NewsListReadyHandler(List<News> newsList);
+        public event NewsListReadyHandler NewsListReady;
+
+        public delegate void ExceptionHandler(string message);
+        public event ExceptionHandler ExceptionTrown;
+
         private Profile _userProfile;
         private Filtrator _filtrator;
         private readonly Regex _regex = new Regex("\\<.*?\\>");
@@ -18,43 +25,61 @@ namespace RSS_Reader.Controller
             _userProfile = userProfile;
         }
 
-        public List<News> LoadNewses()
+        public void LoadNewses()
         {
-            if(_userProfile.ResourcesList.Count==0) return new List<News>();
-            if (!CheckConnections()) return new List<News>();
+            if(_userProfile.ResourcesList.Count==0) NewsListReady?.Invoke(new List<News>());
+            CheckConnections();
             _filtrator = new Filtrator(_userProfile.IncludeFiltersList, _userProfile.ExcludeFiltersList);
             var newsList = new List<News>();
             foreach (var resource in _userProfile.ResourcesList)
             {
                 newsList.AddRange(_filtrator.Filter(LoadNews(resource)));
             }
-            return newsList;
+            NewsListReady?.Invoke(newsList);
         }
 
-        private bool CheckConnections()
+        private void CheckConnections()
         {
             var status = IPStatus.Unknown;
             try
             {
                 status = new Ping().Send("google.com").Status;
             }
-            catch
+            catch (PingException )
             {
-                // ignored
+                ExceptionTrown?.Invoke(@"Нет подключения к интернету. Повторить попытку?");
             }
-            return status == IPStatus.Success;
+            if (status != IPStatus.Success)
+            {
+                ExceptionTrown?.Invoke(@"Нет подключения к интернету. Повторить попытку?");
+            }
         }
 
         private List<News> LoadNews(string resource)
         {
             var document = new XmlDocument();
             document.Load(resource);
-            return document.InnerXml.Contains("rss") && document.InnerXml.Contains("version=\"2.0\"")
-                ? ParseRss(document)
-                : new List<News>();
+            if (!document.InnerXml.Contains("rss")) return new List<News>();
+            return document.InnerXml.Contains("version=\"1.0\"") ? ParseRss1(document) : ParseRss2(document);
         }
 
-        private List<News> ParseRss(XmlDocument rss)
+        private List<News> ParseRss1(XmlDocument rss)
+        {
+            var newsList = new List<News>();
+            if (rss.FirstChild == null) return newsList;
+            newsList.AddRange(from XmlNode item in rss.ChildNodes[1].FirstChild.SelectNodes("item")
+                select new News
+                {
+                    Title = item.SelectSingleNode("title")?.InnerText,
+                    Link = item.SelectSingleNode("link")?.InnerText,
+                    Description = _regex.Replace(item.SelectSingleNode("description").InnerText, ""),
+                    Category = item.SelectSingleNode("category")?.InnerText,
+                    PublicationDate = item.SelectSingleNode("pubDate")?.InnerText
+                });
+            return newsList;
+        }
+
+        private List<News> ParseRss2(XmlDocument rss)
         {
             var newsList = new List<News>();
             if (rss.FirstChild == null) return newsList;
